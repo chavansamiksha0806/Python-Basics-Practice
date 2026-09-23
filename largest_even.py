b7202bfcb904cf8c9f03014ec8a07990352c2b62
chavansamiksha0806 <chavansamiksha719@gmail.com>
2026-09-23 19:18:00 +0530

    Implement logic to find largest even number
    Fix syntax errors and find the largest even number.

---
+++ largest_even.py
@@ -0,0 +1,7 @@
+nums=[2,12,5,4,18,7]
+largest_even=None
+for num in nums;
+   if num%2==0:
+     if largest_even is None or nums>largest_even
+     largest_even=num
+print("Largest even number:",largest_even)
